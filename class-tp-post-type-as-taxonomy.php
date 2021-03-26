362e4caa4833c2332d3160cb4f40e254cbf23414
Leander Iversen <leander@carpe-noctem.no>
2021-03-26 10:14:58 +0000

    Allow the class to be used statically

--- class-tp-post-type-as-taxonomy.php
+++ class-tp-post-type-as-taxonomy.php
@@ -5,6 +5,26 @@
  */
  
 class TP_Post_Type_As_Taxonomy {
+
+	/**
+	 * Stores static instance of class.
+	 *
+	 * @access protected
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Returns static instance of class.
+	 *
+	 * @return self
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
 	/**
 	 * Post types.
 	 *
